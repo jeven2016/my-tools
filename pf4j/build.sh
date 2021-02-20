5051387b323d75d5b69fc188f1f56a5cb973771b
jujucom <jujucom@126.com>
2021-02-20 22:22:21 +0800

    uo

--- pf4j/build.sh
+++ pf4j/build.sh
@@ -2,15 +2,16 @@
 
 gradle clean build
 
-gladle :plugin-spring buildPlugin
+gradle :pf4j:plugin-spring:buildPlugin
+
 
 # for pf4j-test project
 cp plugin-1/build/libs/plugin-1-0.1.jar pf4j-test/lib/
 cp plugin-2/build/libs/plugin-2-0.1.jar pf4j-test/lib/
-cp plugin-spring/build/libs/plugin-spring-0.1.jar pf4j-test/lib/
+cp plugin-spring/build/libs/plugin-spring-0.1.zip pf4j-test/lib/
 
 
 # for pf4j-spring project
 cp plugin-1/build/libs/plugin-1-0.1.jar pf4j-spring/lib/
 cp plugin-2/build/libs/plugin-2-0.1.jar pf4j-spring/lib/
-cp plugin-spring/build/libs/plugin-spring-0.1.jar pf4j-spring/lib/
+cp plugin-spring/build/libs/plugin-spring-0.1.zip pf4j-spring/lib/
--- pf4j/plugin-spring/src/main/java/api/PluginSpringApp.java
+++ pf4j/plugin-spring/src/main/java/api/PluginSpringApp.java
@@ -3,6 +3,10 @@ package api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This class is required for spring boot plugin to build a fat jar,
+ * but it should be excluded in plugin's zip file
+ */
 @SpringBootApplication
 public class PluginSpringApp {
 
